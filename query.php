74c9e04c75c19866f600002779027f054c58b7f1
Brandon Liu <bliu7654@gmail.com>
2016-11-01 22:09:25 -0700

    Added links to search page

--- query.php
+++ query.php
@@ -32,11 +32,11 @@
     $queries = explode(" ", $search);
     //If the array size is 2, that means there are two spaces so we can handle as an AND relation.
     if(count($queries) == 2) {
-      $query = "SELECT first, last, dob FROM Actor WHERE (first LIKE '%$queries[0]%' AND last LIKE '%$queries[1]%') OR first LIKE '%$queries[1]%' AND last LIKE '%$queries[0]%'";
+      $query = "SELECT id, first, last, dob FROM Actor WHERE (first LIKE '%$queries[0]%' AND last LIKE '%$queries[1]%') OR first LIKE '%$queries[1]%' AND last LIKE '%$queries[0]%'";
     }
     //Array size is 1, that means we can run a regular query. Alternatively, if it's greater than 2, then given our actors, we cannot have 2 space in a name.
     else {
-      $query = "SELECT first, last, dob FROM Actor WHERE first LIKE '%$search%' OR last LIKE '%$search%'";
+      $query = "SELECT id, first, last, dob FROM Actor WHERE first LIKE '%$search%' OR last LIKE '%$search%'";
     }
 
     // Run query
@@ -49,19 +49,27 @@
     // Get col names
     echo "<tr align='center'>"; // All col names go in one row
 
-    for ($i = 0; $i < $ncols; $i++) {
-      $cinfo = $rs->fetch_field_direct($i);
-      $cname = $cinfo->name;
-      echo "<td><b>". $cname ."</b></td>";
-    }
+    // for ($i = 0; $i < $ncols; $i++) {
+    //   $cinfo = $rs->fetch_field_direct($i);
+    //   $cname = $cinfo->name;
+    //   echo "<td><b>". $cname ."</b></td>";
+    // }
+    echo"<td><b>Actor Name</b></td>";
+    echo"<td><b>Date of Birth</b></td>";
+
+
     echo "</tr>"; // Close col name row
 
     // Get returned data
     while ($row = $rs->fetch_row()) {
       echo "<tr align='center'>";
-      for ($i = 0; $i < $ncols; $i++) {
+      for ($i = 1; $i < $ncols; $i++) {
         if ($row[$i] == NULL) {
           echo "<td>N/A</td>";
+        }
+        else if ($i == 1) {
+            echo "<td><a href='show_actor.php?id=$row[0]'>". $row[$i] . " " . $row[$i+1] ."</a></td>";
+            $i++;
         }
         else {
           echo "<td>". $row[$i] ."</td>";
@@ -79,7 +87,7 @@
     echo "<h3>Actors with this first or last name:</h3>";
     echo "<table border=1 cellspacing=1 cellpadding=2>";
 
-    $query = "SELECT title, year FROM Movie WHERE title LIKE '%$search%'";
+    $query = "SELECT id, title, year FROM Movie WHERE title LIKE '%$search%'";
     // Run query
     $rs = $db->query($query);
 
@@ -90,7 +98,7 @@
     // Get col names
     echo "<tr align='center'>"; // All col names go in one row
 
-    for ($i = 0; $i < $ncols; $i++) {
+    for ($i = 1; $i < $ncols; $i++) {
       $cinfo = $rs->fetch_field_direct($i);
       $cname = $cinfo->name;
       echo "<td><b>". $cname ."</b></td>";
@@ -100,9 +108,12 @@
     // Get returned data
     while ($row = $rs->fetch_row()) {
       echo "<tr align='center'>";
-      for ($i = 0; $i < $ncols; $i++) {
+      for ($i = 1; $i < $ncols; $i++) {
         if ($row[$i] == NULL) {
           echo "<td>N/A</td>";
+        }
+        else if($i == 1) {
+          echo"<td><a href='show_movie.php?id=$row[0]'>" . $row[$i] . " " . $row[$i+1] . "</a></td>";
         }
         else {
           echo "<td>". $row[$i] ."</td>";
