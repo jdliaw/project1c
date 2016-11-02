420bb1a7e1f87cd22e11aced36dfaf47cdd79041
jdliaw <jdliaw@ucla.edu>
2016-11-02 00:08:40 -0700

    Update navbar links

--- navbar.php
+++ navbar.php
@@ -1,13 +1,13 @@
 <link href="navbar.css" rel="stylesheet">
 
 <nav>
-  <div class="navitem left"> <a href="/~cs143/project1c/">Home</a></div>
+  <div class="navitem left"> <a href="splash.php">Home</a></div>
   <div class="navitem right">
     <div class="navbar dropdown">
-      <a class="drop-btn" href="/~cs143/project1c/">Add New Content</a><br />
+      <a class="drop-btn" href="input.php">Add New Content</a><br />
     </div>
   </div>
   <div class="navitem right">
-    <a class="navbar" href="/~cs143/project1c/query.php">Search Actor/Movie</a>
+    <a class="navbar" href="query.php">Search Actor/Movie</a>
   </div>
 </nav>
