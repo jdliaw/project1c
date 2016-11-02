efb47357c37d78258b9df59f43d6cbd9e6f5b5be
jdliaw <jdliaw@ucla.edu>
2016-11-01 23:49:18 -0700

    Landing page for add pages

---
+++ input.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="input.css">
+<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+
+<div class="container">
+  <div class="title">
+    <p>Add a record</p>
+    <hr>
+    <button onclick="location.href='actor-director.php'">Actor/Director</button>
+    <button onclick="location.href='movie-input.php'">Movie</button><br />
+    <button onclick="location.href='actor-movie.php'">Actor Role</button>
+    <button onclick="location.href='director-movie.php'">Director of Movie</button><br />
+    <button onclick="location.href='movie-review.php'">Movie Review</button>
+  </div>
+</div>
--- splash.php
+++ splash.php
@@ -4,7 +4,8 @@
 <div class="container">
   <div class="title">
     Welcome to MoviePedia!<br />
-    <button type="button" onclick="location.href='actor-director.php'">Add a record</button>
+    <hr>
+    <button type="button" onclick="location.href='input.php'">Add a record</button>
     <button type="button" onclick="location.href='query.php'">Browse</button>
   </div>
 </div>
